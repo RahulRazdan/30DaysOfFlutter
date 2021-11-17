01e09486ec33839f3e4761346d1267271fb31b5a
Rahul Razdan <RahulRazdan@fico.com>
2021-11-17 18:37:38 +0530

    Day13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_application/models/billu.dart';
 import 'package:flutter_application/widgets/drawer.dart';
 import 'package:flutter_application/widgets/item_widget.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -34,23 +35,97 @@ class _HomePageState extends State<HomePage> {
     //final dummyList = List.generate(10, (index) => BilluModel.items[0]);
 
     return Scaffold(
-      appBar: AppBar(title: const Text("Billu App")),
-      body: (BilluModel.items != null && BilluModel.items.isNotEmpty)
-          ? GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, mainAxisSpacing: 16, crossAxisSpacing: 16),
-              itemCount: BilluModel.items.length,
-              itemBuilder: (context, index) {
-                return ItemWidget(item: BilluModel.items[index]);
-              },
-            )
-          : const Center(
-              child: CircularProgressIndicator(
-                color: Colors.black,
-              ),
-            ),
-      backgroundColor: Colors.amberAccent,
-      drawer: const MyDrawer(),
+        body: SafeArea(
+      child: Container(
+        padding: Vx.m32,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const CatalogHeader(),
+            if (BilluModel.items.isNotEmpty)
+              const CatalogList().expand()
+            else
+              const Center(child: CircularProgressIndicator())
+          ],
+        ),
+      ),
+    ));
+  }
+}
+
+class CatalogHeader extends StatelessWidget {
+  const CatalogHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        "Billu App".text.xl5.bold.color(Colors.blueAccent).make(),
+        "Always Trending".text.xl2.make()
+      ],
     );
+  }
+}
+
+class CatalogList extends StatelessWidget {
+  const CatalogList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          return CatalogItem(catalogItem: BilluModel.items[index]);
+        },
+        itemCount: BilluModel.items.length);
+  }
+}
+
+class CatalogItem extends StatelessWidget {
+  final Item catalogItem;
+  const CatalogItem({Key? key, required this.catalogItem}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return VxBox(
+        child: Row(
+      children: [
+        CatalogImage(image: catalogItem.image),
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            catalogItem.name.text.lg.bold.make(),
+            catalogItem.desc.text.textStyle(context.captionStyle!).make(),
+            10.heightBox,
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceBetween,
+              buttonPadding: EdgeInsets.zero,
+              children: [
+                "\$${catalogItem.price}".text.bold.xl.make(),
+                ElevatedButton(
+                    onPressed: () {},
+                    child: "Buy".text.make(),
+                    style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all(const StadiumBorder())))
+              ],
+            ).pOnly(right: 8)
+          ],
+        ))
+      ],
+    )).white.roundedSM.square(150).make().py16();
+  }
+}
+
+class CatalogImage extends StatelessWidget {
+  final image;
+  const CatalogImage({Key? key, required this.image}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(image).box.rounded.p8.color(Colors.amber).make().p16();
   }
 }
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -5,7 +5,7 @@ import 'package:google_fonts/google_fonts.dart';
 class MyTheme {
   static ThemeData get lightTheme => ThemeData(
       primarySwatch: Colors.amber,
-      fontFamily: GoogleFonts.lato().fontFamily,
+      fontFamily: GoogleFonts.poppins().fontFamily,
       appBarTheme: const AppBarTheme(
         elevation: 0.0,
         backgroundColor: Colors.amberAccent,
